c47f17e075cdd4ede4664c6c240db3621f423102
Gurjyot <gurjyot-mini@Gurjyots-Mac-mini.local>
2025-07-20 02:48:48 -0400

    Added python code

--- src/app.py
+++ src/app.py
@@ -1,1 +1,13 @@
-print("DevOps World")
+from flask import Flask
+
+# Create a Flask web server
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello, World from Kubernetes and Helm!\n'
+
+if __name__ == '__main__':
+    # Run the app on 0.0.0.0 to be publicly accessible from within the container
+    app.run(host='0.0.0.0', port=8080)
